4480d8818288c366cd3dc47a3906553c8639de0c
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-11-12 19:05:29 +0530

    aa

--- components/MOBILE/TestimonialsMOB.vue
+++ components/MOBILE/TestimonialsMOB.vue
@@ -54,6 +54,27 @@
         <span
           class="w-full flex items-center justify-start gap-[.6vw] px-[1vw] py-[1vh] text-white font-popins text-[.8rem]"
         >
+          <NuxtImg class="h-[1.2rem]" src="/icons/homestarsIcon.png" alt="#" />
+          Posted on HomeStars
+        </span>
+        <a
+          target="_blank"
+          href="https://homestars.com/companies/2941871-calico-design-renovation/reviews/1090483"
+          class="text-[#ededed] font-popins text-[.8rem] flex items-center gap-[.4vw] hover:underline"
+        >
+          SOURCE
+          <Icon icon="tabler:external-link" class="text-[1rem]" />
+        </a>
+      </div>
+      <NuxtImg class="h-max w-full object-contain" src="./hRev3.png" alt="#" />
+    </div>
+    <div
+      class="h-max w-full overflow-hidden rounded-md bg-[#222] block flex flex-col justify-between border-[2px] border-[#222]"
+    >
+      <div class="h-max w-full flex justify-between px-[2vw] items-center">
+        <span
+          class="w-full flex items-center justify-start gap-[.6vw] px-[1vw] py-[1vh] text-white font-popins text-[.8rem]"
+        >
           <NuxtImg class="h-[1.2rem]" src="/icons/gmaps.svg" alt="#" />
           Posted on Maps
         </span>
