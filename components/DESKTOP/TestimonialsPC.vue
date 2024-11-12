f3208c24bf4a0e8950e91eaaa1569baddbb3ad92
Riswin Mohammed <123391105+rizwyy@users.noreply.github.com>
2024-11-12 19:06:07 +0530

    asd

--- components/DESKTOP/TestimonialsPC.vue
+++ components/DESKTOP/TestimonialsPC.vue
@@ -111,6 +111,33 @@
             </span>
             <a
               target="_blank"
+              href="https://homestars.com/companies/2941871-calico-design-renovation/reviews/1090483"
+              class="text-[#ededed] font-popins text-[.8rem] flex items-center gap-[.4vw] hover:underline"
+            >
+              SOURCE
+              <Icon icon="tabler:external-link" class="text-[1rem]" />
+            </a>
+          </div>
+          <NuxtImg
+            class="h-[38rem] w-full object-fit"
+            src="./hRev3.png"
+            alt="#"
+          />
+        </div>
+        <div class="h-max w-max overflow-hidden rounded-md bg-[#222] block">
+          <div class="h-max w-full flex justify-between pr-[1vw] items-center">
+            <span
+              class="w-full flex items-center justify-start gap-[.6vw] px-[1vw] py-[1vh] text-white font-popins"
+            >
+              <NuxtImg
+                class="h-[2rem]"
+                src="/icons/homestarsIcon.png"
+                alt="#"
+              />
+              Posted on HomeStars
+            </span>
+            <a
+              target="_blank"
               href="https://homestars.com/companies/2941871-calico-design-renovation/reviews/1232961"
               class="text-[#ededed] font-popins text-[.8rem] flex items-center gap-[.4vw] hover:underline"
             >
